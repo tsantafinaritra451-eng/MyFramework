e3bb9dc486bbe968a48601b5f5d2df509da9e92b
Noahmathieu <tsantafinaritra451@gmail.com>
2026-06-22 22:09:04 +0300

    sprint 2 ok

--- src/main/java/presentation/FrontControllerServlet.java
+++ src/main/java/presentation/FrontControllerServlet.java
@@ -1,8 +1,10 @@
 package presentation;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.lang.reflect.Method;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -11,27 +13,37 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 import mg.itu.tsanta.annotation.Controller;
+import mg.itu.tsanta.annotation.Url;
 
 public class FrontControllerServlet extends HttpServlet {
         public FrontControllerServlet() {
                 super();
         }
 
-        private List<String> listControllers = new ArrayList<>();
-
+        private List<Method> listUrl = new ArrayList<>();
+     
         @Override
         public void init() throws ServletException {
                 try {
                         List<String> toutesClasses = Utilitaire.ScanneClass("controllers");
+
                         for (String className : toutesClasses) {
                                 Class<?> clazz = Class.forName(className);
-                                if (clazz.isAnnotationPresent(Controller.class)) {
-                                        this.listControllers.add(className);
+                                Method[] toutesMethodes = clazz.getDeclaredMethods();
+                                for (Method toutMethode : toutesMethodes) {
+                                        if (toutMethode.isAnnotationPresent(Url.class)) {
+                                                Url annotation = toutMethode.getAnnotation(Url.class);
+                                                String route = annotation.value();
+                                                this.listUrl.add(toutMethode);
+
+                                        }
+
                                 }
+
                         }
-                        
-                        System.out.println("Framework OK " + this.listControllers.size());                  
-                        
+
+                        System.out.println("Framework OK " + this.listUrl.size());
+
                 } catch (Exception e) {
                         throw new ServletException("Erreur initialisation", e);
                 }
@@ -40,14 +52,39 @@ public class FrontControllerServlet extends HttpServlet {
         protected void processRequest(HttpServletRequest request, HttpServletResponse response)
                         throws ServletException, IOException {
                 response.setContentType("text/plain");
-                String url = request.getRequestURL().toString();
+                PrintWriter out = response.getWriter();
+                String url = request.getPathInfo();
 
-                response.getWriter().println("URL demandée : " + url);
-                response.getWriter().println("--- Liste des contrôleurs détectés par le framework ---");
+                for (Method listeUrl : listUrl) {
+                        Url annotation = listeUrl.getAnnotation(Url.class);
+                        String route = annotation.value();
+                        if (route.equals(url)) {
+                                String nomClass = listeUrl.getDeclaringClass().getName();
+                                String nomMethode = listeUrl.getName();
 
-                for (String ctrl : this.listControllers) {
-                        response.getWriter().println("- " + ctrl);
+                                out.println("Url trouver");
+                                out.println("methode:" + nomMethode);
+                                out.println("class:" + nomClass);
+                                return;
+
+                        }
+
                 }
+                out.println("cette url" + url + "ne contient pas annotation");
+                out.println("les url disponible avec leur methode et classe sonr:");
+
+                for (Method urlDiso : listUrl) {
+                        Url annotation = urlDiso.getAnnotation(Url.class);
+                        String routeDispo = annotation.value();
+                        String nomClassDispo= urlDiso.getDeclaringClass().getName();
+                        String nomMethodeDispo = urlDiso.getName();
+
+                        out.println("   URL      : " + routeDispo);
+                        out.println("   Classe   : " + nomMethodeDispo);
+                        out.println("   Methode  : " + nomClassDispo);
+
+                }
+
         }
 
         @Override
